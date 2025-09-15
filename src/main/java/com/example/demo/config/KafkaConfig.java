8582f036bf02b89ccc1b6b629f388681965d5257
JuKoNuNu <127651508+JuKoNuNu@users.noreply.github.com>
2025-09-15 17:58:01 +0900

    kafkaAIaa#24

--- src/main/java/com/example/demo/config/KafkaConfig.java
+++ src/main/java/com/example/demo/config/KafkaConfig.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-    @Value("${kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
     /**
